ba49084aa9d1b1916a2040bf517f8b74ade508fe
isaac rubio <isaacamaru.rubio@elev.ga.ntig.se>
2024-09-10 10:08:24 +0200

    b

---
+++ monster.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class monster {
+    
+}
+
+public class monster{
+    public int health = 50;
+  public int size = 3;
+  public String Blatte = "Monster";
+}
+
---
+++ paladin.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+}
+
+public class paladin{
+    public int health = 100;
+    public int attack = 10;
+    public String Gahbe = "Paladin";
+}
